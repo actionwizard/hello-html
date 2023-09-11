5596686d64b38343afc6809819b1e613872a75b1
Anish Kumar <anishykumar42@gmail.com>
2023-09-11 17:20:33 +0530

    added the button functionality to the conversion site

--- unitConversion/js/unitConversionScript.js
+++ unitConversion/js/unitConversionScript.js
@@ -1,17 +1,17 @@
 function temperature() {
   //To convert celcius to farenheit
   //(CEL * 9/5) + 32
-  var c = document.getElementById("celsius").value;
-  var f = (c * 9) / 5 + 32;
-  document.getElementById("fahrenheit").value = f;
+  let c = document.getElementById("c").value;
+  let f = (c * 9) / 5 + 32;
+  document.getElementById("f").value = f;
 }
 
 function weight() {
   //To convert KGs to Pounds
   // KG * 2.2
-  var kg = document.getElementById("kilo").value;
-  var p = kg * 2.2;
-  document.getElementById("pounds").value = p;
+  let kg = document.getElementById("kg").value;
+  let lbs = kg * 2.2;
+  document.getElementById("lbs").value = lbs;
 }
 
 function distance() {
@@ -19,5 +19,5 @@ function distance() {
   // KM * 0.62137
   var km = document.getElementById("km").value;
   var m = km * 0.62137;
-  document.getElementById("miles").value = m;
+  document.getElementById("m").value = m;
 }
